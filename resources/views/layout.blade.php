1ec5f0260441f2c04cb474dd9fb013f8bd05a9b1
Just <166110883+angesomhailu@users.noreply.github.com>
2025-01-26 10:21:53 +0300

    another commit

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -66,25 +66,6 @@ div.content {
     <span class="navbar-toggler-icon"></span>
   </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto ">
-      <li class="nav-item">
-        <a class="nav-link active text-primary" href="#"><h4>Contact</h4></a>
-      </li>
-      
-      <li class="nav-item">
-        <a class="nav-link active text-primary" href="#"><h4>About</h4></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link active text-primary" href="#"><h4>Services</h4></a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-   
-  </div>
 </nav>
 </div>
 </div>
