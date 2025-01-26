84e616621c5d30d197a0a0012326082a42e76c68
Just <166110883+angesomhailu@users.noreply.github.com>
2025-01-26 10:02:34 +0300

    another commit

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -83,6 +83,7 @@ div.content {
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
+   
   </div>
 </nav>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\StudentController;
@@ -13,10 +12,6 @@ Route::get('/', function () {
     return view('layout');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::resource("/students", StudentController::class);
 Route::resource("/courses", CourseController::class);
 Route::resource("/batches", BatchController::class);
@@ -25,10 +20,4 @@ Route::resource("/payments", PaymentController::class);
 Route::get('report/report1/{pid}',[ReportController::class,'report1']);
 
 
-Route::middleware('auth')->group(function () {
-Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
-require __DIR__.'/auth.php';
