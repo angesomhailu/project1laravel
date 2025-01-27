df285d32abb5eb296dbeefe9f13eb2dfa7e93f45
Just <166110883+angesomhailu@users.noreply.github.com>
2025-01-27 19:33:22 +0300

    another commit

--- tests/Unit/StudentRegistrationTest.php
+++ tests/Unit/StudentRegistrationTest.php
@@ -14,17 +14,17 @@ class StudentRegistrationTest extends TestCase
     public function it_can_create_a_student()
     {
         $studentData = [
-            'name' => 'John Doe',
-            'email' => 'johndoe@example.com',
-            'student_id' => '123456',
-            'course' => 'Computer Science',
+            'name' => 'angesom bahre',
+            'email' => 'angesomhailu1414@gmail.com',
+            'student_id' => '1',
+            'course' => 'java',
         ];
 
         $students = Student::create($studentData);
 
         $this->assertDatabaseHas('students', [
-            'name' => 'John Doe',
-            'email' => 'johndoe@example.com',
+            'name' => 'angesom bahre',
+            'email' => 'angesomhailu1414@gmail.com',
         ]);
     }
 }
